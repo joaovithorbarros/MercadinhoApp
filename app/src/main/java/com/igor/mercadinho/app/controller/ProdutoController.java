a97c9dd49e37c1ad89b5f8e43ebd63a6a2bf993a
JIgorSilva <jigorsilva@hotmail.com>
2024-12-21 15:10:34 -0300

    Criação contorller para busca de produto por ad. adicionando o método Optional<Produtos> findById(int id) na interface ProdutoRepository. (#6)
    permite buscar um produto específico pelo seu identificador (ID).

--- app/src/main/java/com/igor/mercadinho/app/controller/ProdutoController.java
+++ app/src/main/java/com/igor/mercadinho/app/controller/ProdutoController.java
@@ -1,19 +1,24 @@
 package com.igor.mercadinho.app.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.igor.mercadinho.app.exception.ProdutoResouceNotFoundException;
 import com.igor.mercadinho.app.model.Produtos;
 import com.igor.mercadinho.app.repository.ProdutoRepository;
 
 @RestController
-@RequestMapping("/api/produtos")
+@RequestMapping("/api/produto")
 public class ProdutoController {
 
     @Autowired
@@ -27,5 +32,12 @@ public class ProdutoController {
     @PostMapping
     public Produtos criarProduto(@RequestBody Produtos produto) {
         return produtoRepository.save(produto);
+    }
+
+    @PutMapping("buscar/{id}")
+    public ResponseEntity<Produtos> buscarProdutoPorId(@PathVariable int id) {
+        Produtos produtos = produtoRepository.findById(id)
+                .orElseThrow(() -> new ProdutoResouceNotFoundException("ID do produto não existe "  +id));
+        return ResponseEntity.ok(produtos);
     }
 }
--- app/src/main/java/com/igor/mercadinho/app/repository/ProdutoRepository.java
+++ app/src/main/java/com/igor/mercadinho/app/repository/ProdutoRepository.java
@@ -1,9 +1,12 @@
 package com.igor.mercadinho.app.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.igor.mercadinho.app.model.Produtos;
 
 public interface ProdutoRepository extends JpaRepository<Produtos, Long> {
-    // Métodos customizados para consultas, se necessário
+
+    Optional<Produtos> findById(int id);
 }
