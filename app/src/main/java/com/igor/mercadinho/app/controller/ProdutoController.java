a920df6dfd5fd432a7f3efeffe4c4fee0e69fd8d
JIgorSilva <jigorsilva@hotmail.com>
2024-12-22 18:18:32 -0300

    correção do erro de retorno do metodo. (+ -> adicão do esquema do banco para teste). (#8)

--- app/src/main/java/com/igor/mercadinho/app/controller/ProdutoController.java
+++ app/src/main/java/com/igor/mercadinho/app/controller/ProdutoController.java
@@ -1,14 +1,12 @@
 package com.igor.mercadinho.app.controller;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,10 +32,15 @@ public class ProdutoController {
         return produtoRepository.save(produto);
     }
 
-    @PutMapping("buscar/{id}")
+    @GetMapping("buscar/{id}")
     public ResponseEntity<Produtos> buscarProdutoPorId(@PathVariable int id) {
         Produtos produtos = produtoRepository.findById(id)
-                .orElseThrow(() -> new ProdutoResouceNotFoundException("ID do produto não existe "  +id));
+                .orElseThrow(() -> new ProdutoResouceNotFoundException("ID do produto não existe " + id));
         return ResponseEntity.ok(produtos);
     }
+    /*
+     * criar metodo que retorna uma array list de todos os produtos contendo aquela palavra,  ignorando escrita
+     */
+   
+
 }
