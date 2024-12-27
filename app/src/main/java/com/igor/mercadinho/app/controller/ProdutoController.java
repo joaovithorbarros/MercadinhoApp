fef05304fd1f62f441f12d6450b5688800bf4445
JIgorSilva <jigorsilva@hotmail.com>
2024-12-27 12:12:46 -0300

    Removento injeção de dependencia do repository na controller. Adição da anotatation para repository. (#16)

--- app/src/main/java/com/igor/mercadinho/app/controller/ProdutoController.java
+++ app/src/main/java/com/igor/mercadinho/app/controller/ProdutoController.java
@@ -9,34 +9,34 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.igor.mercadinho.app.exception.ProdutoResouceNotFoundException;
 import com.igor.mercadinho.app.model.Produtos;
-import com.igor.mercadinho.app.repository.ProdutoRepository;
+import com.igor.mercadinho.app.services.ProdutosService;
 
 @RestController
 @RequestMapping("/api/produto")
 public class ProdutoController {
 
     @Autowired
-    private ProdutoRepository produtoRepository;
+    private ProdutosService produtosService;
 
-    @GetMapping("buscar")
+    @GetMapping("/lista")
     public ResponseEntity<List<Produtos>> listarProdutos() {
-        List<Produtos> produtosLista = produtoRepository.findAll();
+        List<Produtos> produtosLista = produtosService.listarTodosProdutos();
         return ResponseEntity.ok(produtosLista);
     }
 
-    @PostMapping()
-    public Produtos criarProduto(@RequestBody Produtos produto) {
-        return produtoRepository.save(produto);
+    @PostMapping("criar/{produto}")
+    public ResponseEntity<Produtos> criarProduto(@RequestBody Produtos produto) {
+        produtosService.criarProduto(produto);
+        return ResponseEntity.ok(produto);
     }
 
-    @GetMapping("buscar/{id}")
-    public ResponseEntity<Produtos> buscarProdutoPorId(@PathVariable int id) {
-        Produtos produtos = produtoRepository.findById(id)
-                .orElseThrow(() -> new ProdutoResouceNotFoundException("ID do produto não existe " + id));
+    @GetMapping("buscar/id")
+    public ResponseEntity<Produtos> buscarProdutoPorId(@RequestParam int id) {
+        Produtos produtos = produtosService.buscarProdutoPorId(id);
         return ResponseEntity.ok(produtos);
     }
     /*
--- app/src/main/java/com/igor/mercadinho/app/repository/ProdutoRepository.java
+++ app/src/main/java/com/igor/mercadinho/app/repository/ProdutoRepository.java
@@ -3,10 +3,13 @@ package com.igor.mercadinho.app.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.igor.mercadinho.app.model.Produtos;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produtos, Long> {
 
     Optional<Produtos> findById(int id);
+
 }
